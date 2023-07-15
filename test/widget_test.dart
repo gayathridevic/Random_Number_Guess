2f87c8de78ba0950cb02a6bf9c812ba606568485
Rangarajan Padur Sivaraman <rangarajanps@gmail.com>
2023-07-15 08:11:10 +0100

    review and fix display of random number

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:numberguessinggame/main.dart';
+import 'package:numberguessinggame/homepage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
